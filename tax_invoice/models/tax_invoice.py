92d4d58d0aabaab092d16fd66922e69c4a0fe73b
宣一敏 <freemanxuan@163.com>
2017-12-21 22:27:16 +0800

    修正BUG

--- tax_invoice/models/tax_invoice.py
+++ tax_invoice/models/tax_invoice.py
@@ -27,26 +27,26 @@ import base64
 import datetime
 import time
 import re
-
-#from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
+from PIL import Image
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 # 字段只读状态
 READONLY_STATES = {
         'done': [('readonly', True)],
     }
 
-TAX_TYPE = [('17', u'17%税率'),
-            ('13', u'13%税率'),
-            ('11', u'11%税率'),
-            ('6', u'6%税率'),
-            ('5', u'5%税率'),
-            ('3', u'3%税率'),
-            ('1.5', u'1.5%税率'),]
-
 #初始化chrome
 #options = webdriver.ChromeOptions()
 #options.add_argument('--explicitly-allowed-ports=6000,556')
+
+#加载插件
+#profile=webdriver.FirefoxProfile()
+#profile.add_extension('c:\\firebug-2.0.8-fx.xpi')
+#激活插件
+#profile.set_preference("extensions.firebug.allPagesActivation", "on")
+firefox_path = r"C:\Program Files (x86)\Mozilla Firefox\geckodriver.exe"
+browser = False
 
 class tax_invoice(models.Model):
     '''税务发票'''
@@ -59,7 +59,7 @@ class tax_invoice(models.Model):
         ondelete='restrict',
         required=True,
         states=READONLY_STATES)
-    line_ids = fields.One2many('tax.invoice.line', 'order_id', u'折旧明细行',
+    line_ids = fields.One2many('tax.invoice.line', 'order_id', u'明细发票行',
                                states=READONLY_STATES, copy=False)
     state = fields.Selection([('draft', u'草稿'),
                               ('done', u'已结束')], u'状态', default='draft')
@@ -79,6 +79,39 @@ class tax_invoice(models.Model):
                 total = total +line.invoice_tax
         self.tax_amount = total
 
+    #由发票生成采购订单
+    @api.one
+    def invoice_to_buy(self):
+        for i in self.line_ids:
+            buy_id = self.env['buy.order'].create({
+                'partner_id': i.partner_id.id,
+                'date': i.invoice_open_date,
+                'planned_date': i.invoice_open_date,
+                'warehouse_dest_id': self.env['warehouse'].search([('type', '=', 'stock')]).id,
+            })
+            if i.invoice_line_detail:
+                for lines in i.line_ids:
+                    goods_id = self.env['goods'].search([('name', '=', lines.product_name)])
+                    self.env['buy.order.line'].create({
+                        'goods_id':goods_id.id,
+                        'order_id':buy_id.id,
+                        'uom_id':goods_id.uom_id.id,
+                        'quantity':lines.product_count,
+                        'price':lines.product_price,
+                        'price_taxed':round(lines.product_price*(1+lines.product_tax_rate/100),2),
+                        'amount':lines.product_amount,
+                        'tax_rate':lines.product_tax_rate,
+                        'tax_amount':lines.product_tax,
+                    })
+                self.buy_id = buy_id
+                buy_id.buy_order_done()
+                delivery_id = self.env['buy.receipt'].search([
+                    ('order_id', '=', buy_id.id)])
+                delivery_id.buy_receipt_done()
+                invoice_id = self.env['money.invoice'].search([
+                    ('name', '=', delivery_id.name)])
+                invoice_id.bill_number = i.invoice_name
+
     #结束本期发票，然后确认money_invoice
     @api.one
     def tax_invoice_done(self):
@@ -86,6 +119,8 @@ class tax_invoice(models.Model):
         for line in self.line_ids:
             if not line.partner_id:
                 raise UserError(u'税号%s没找到到供应商，请去供应商处维护！'%line.partner_code)
+            if not line.is_verified:
+                raise UserError(u'发票%s未核验，请核验！'%line.invoice_name)
             if not line.money_invoice_ids:
                 raise UserError(u'%s的认证发票%s未与系统相关联请关联'%(line.partner_id.name,line.invoice_name))
             amount = line.invoice_amount
@@ -258,10 +293,101 @@ class tax_invoice_line(models.Model):
     is_verified = fields.Boolean(u'已核验')
     invoice_line_detail=fields.One2many('tax.invoice.line.detail', 'line_id', u'发票明细行',
                                copy=False)
+    buy_id = fields.Many2one('buy.order', u'采购订单号', copy=False, readonly=True,
+                             ondelete='cascade',
+                             help=u'产生的采购订单')
+    image = fields.Binary(u'图片', attachment=True)
+    img_note = fields.Char(u"填写要求")
+    img_code = fields.Char(u'验证码')
 
     _sql_constraints = [
         ('unique_start_date', 'unique (invoice_code, invoice_name)', u'发票代码+发票号码不能相同!'),
     ]
+
+    def get_img_code(self):
+        global browser
+        if not browser:
+            browser = webdriver.Firefox(executable_path=firefox_path)
+            browser.maximize_window()
+        url = "https://inv-veri.chinatax.gov.cn/"
+        browser.get(url)
+        date = self.invoice_open_date.replace('-','')
+        browser.find_element_by_id("fpdm").send_keys(self.invoice_code)
+        browser.find_element_by_id("fphm").send_keys(self.invoice_name)
+        date_in = browser.find_element_by_id("kprq")
+        date_in.clear()
+        date_in.send_keys(date)
+        browser.find_element_by_id("kjje").send_keys(str(self.invoice_amount))
+        time.sleep(2)
+        #取图片
+        imgelement = browser.find_element_by_id("yzm_img")
+        img_code = imgelement.get_attribute('src')[22:]
+        #取图片说明
+        img_note = browser.find_element_by_id("yzminfo").text
+        self.write({'image': img_code,'img_note':img_note})
+
+    def to_verified(self):
+        if not browser:
+            raise UserError(u'请重新取验证码')
+        yzm = browser.find_element_by_id("yzm")
+        yzm.clear()
+        yzm.send_keys(self.img_code)
+        button = browser.find_element_by_id("checkfp")
+        button.click()
+        time.sleep(6)
+        #如果有JS弹出窗，则由gooderp返回给用户
+        try:
+            browser.find_element_by_id("bg_div")
+        except:
+            alert = browser.switch_to_alert()
+            text = browser.find_element_by_id("popup_message").text
+            raise UserError(u'提示！%s' % text)
+        try:
+            button_mx = browser.find_element_by_id("showmx")
+            button_mx.click()
+            time.sleep(6)
+            tr_line = browser.find_elements_by_xpath("//tr [@id='tab_head_mx']/../tr")
+            for tr in tr_line[1:-3]:
+                # 将每一个tr的数据根据td查询出来，返回结果为list对象,第一行和最后三行不要
+                print tr.text
+                table_td_list = tr.find_elements_by_tag_name("td")
+                row_list = []
+                for td in table_td_list:  # 遍历每一个td
+                    print td.text
+                    row_list.append(td.text)  # 取出表格的数据，并放入行列表里
+                self.env['tax.invoice.line.detail'].create({
+                    'line_id':self.id,
+                    'product_name': row_list[1],
+                    'product_type': row_list[2] or '',
+                    'product_unit': row_list[-6] or '',
+                    'product_count': row_list[-5] or '',
+                    'product_price': row_list[-4] or '',
+                    'product_amount': row_list[-3] or '0',
+                    'product_tax_rate':row_list[-2].replace('%','') or '0',
+                    'product_tax': row_list[-1] or '0',
+                })
+        except:
+            tr_line = browser.find_elements_by_xpath("//tr [@id='tab_head_zp']/../tr")
+            for tr in tr_line[1:-2]:
+                # 将每一个tr的数据根据td查询出来，返回结果为list对象,第一行和最后二行不要
+                print tr.text
+                table_td_list = tr.find_elements_by_tag_name("td")
+                row_list = []
+                for td in table_td_list:  # 遍历每一个td
+                    print td.text
+                    row_list.append(td.text)  # 取出表格的数据，并放入行列表里
+                self.env['tax.invoice.line.detail'].create({
+                    'line_id':self.id,
+                    'product_name': row_list[0],
+                    'product_type': row_list[1] or '',
+                    'product_unit': row_list[-6] or '',
+                    'product_count': row_list[-5] or '',
+                    'product_price': row_list[-4] or '',
+                    'product_amount': row_list[-3] or '0',
+                    'product_tax_rate':row_list[-2].replace('%',''),
+                    'product_tax': row_list[-1] or '0',
+                })
+        self.write({'is_verified': 1})
 
 class tax_invoice_line_detail(models.Model):
     _name = 'tax.invoice.line.detail'
@@ -269,14 +395,14 @@ class tax_invoice_line_detail(models.Model):
     _rec_name='product_name'
 
     line_id = fields.Many2one('tax.invoice.line', u'认证发票明细',help=u'认证发票明细',copy=False)
-    product_name = fields.Char(string="货物名称")
-    product_type = fields.Char(string="规格型号")
-    product_unit = fields.Char(string="单位")
-    product_count = fields.Float(string="数量")
-    product_price = fields.Float(string="价格")
-    product_amount = fields.Float(string="金额")
-    product_tax_rate = fields.Float(string="税率")
-    product_tax = fields.Float(string="税额")
+    product_name = fields.Char(u"货物名称")
+    product_type = fields.Char(u"规格型号")
+    product_unit = fields.Char(u"单位")
+    product_count = fields.Float(u"数量")
+    product_price = fields.Float(u"价格")
+    product_amount = fields.Float(u"金额")
+    product_tax_rate = fields.Float(u"税率")
+    product_tax = fields.Float(u"税额")
 
 class create_invoice_line_wizard(models.TransientModel):
     _name = 'create.invoice.line.wizard'
@@ -284,7 +410,6 @@ class create_invoice_line_wizard(models.TransientModel):
 
     excel = fields.Binary(u'导入认证系统导出的excel文件',)
 
-    @api.one
     def create_invoice_line(self):
         if not self.env.context.get('active_id'):
             return
@@ -295,8 +420,7 @@ class create_invoice_line_wizard(models.TransientModel):
         tax_invoice = self.env['tax.invoice'].browse(self.env.context.get('active_id'))
         if not tax_invoice:
             return {}
-        xls_data = xlrd.open_workbook(
-                file_contents=base64.decodestring(self.excel))
+        xls_data = xlrd.open_workbook(file_contents=base64.decodestring(self.excel))
         table = xls_data.sheets()[0]
         ncows = table.nrows
         ncols = 0
@@ -323,6 +447,7 @@ class create_invoice_line_wizard(models.TransientModel):
             else:
                 partner_id = self.env['partner'].search([
                                  ('name', '=', in_xls_data.get(u'销方名称'))])
+
             if in_xls_data.get(u'发票代码'):
                 code = in_xls_data.get(u'发票代码')
                 is_verified = True
@@ -330,7 +455,17 @@ class create_invoice_line_wizard(models.TransientModel):
                     b = code[7:8]
                     if b == '1' or b == '5':
                         is_verified = False
-
+            '''
+            if not partner_id.id:
+                partner_id = self.env['partner'].create({
+                    'name': in_xls_data.get(u'销方名称'),
+                    'main_mobile': in_xls_data.get(u'销方税号'),
+                    'tax_num': in_xls_data.get(u'销方税号'),
+                    's_category_id': self.env['core.category'].search([
+                        '&', ('type', '=', 'supplier'), ('note', '=', u'默认供应商类别')]).id,
+                    'computer_import': True,
+                })
+            '''
             self.env['tax.invoice.line'].create({
                 'partner_code': str(in_xls_data.get(u'销方税号')),
                 'partner_id': partner_id.id,
